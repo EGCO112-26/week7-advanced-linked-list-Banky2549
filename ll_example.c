c5da70b5b671f8b6fb0d4cee8d646a6c68789abf
Banky <bankdoctor2549@gmail.com>
2026-02-16 10:09:08 +0000

    w7

--- ll_example.c
+++ ll_example.c
@@ -2,12 +2,14 @@
 // Inserting and deleting nodes in a list
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include "ll.h"
 int main( void )
 { 
    LLPtr startPtr = NULL; // initially there are no nodes
    unsigned int choice; // user's choice
    int item; // char entered by user
+   char name[50];
 
    instructions(); // display the menu
    printf( "%s", "? " );
@@ -18,10 +20,12 @@ int main( void )
 
       switch ( choice ) { 
          case 1:
-            printf( "%s", "Enter a number: " );
+            printf( "%s", "Enter id and name: " );
             scanf( "%d", &item );
-            insert( &startPtr, item ); // insert item in list
+            scanf( "%s", name );
+            insert( &startPtr, item ,name ); // insert item in list
             printList( startPtr );
+            printListR( startPtr );
             break;
          case 2: // delete an element
             // if list is not empty
@@ -30,9 +34,10 @@ int main( void )
                scanf( "%d", &item );
 
                // if character is found, remove it
-               if ( deletes( &startPtr, item ) ) { // remove item
+               if ( deletes( &startPtr, item )==item ) { // remove item
                   printf( "%d deleted.\n", item );
                   printList( startPtr );
+                  printListR( startPtr );
                } // end if
                else {
                   printf( "%d not found.\n\n", item );
@@ -53,5 +58,11 @@ int main( void )
       scanf( "%u", &choice );
    } // end while
   /* Clear all nodes at the end of nodes*/
+   puts("Clear all nodes");
+         while(startPtr!=NULL)
+         {
+            printf("delete %d\n",startPtr->data);
+            deletes( &startPtr, startPtr->data);
+         }
    puts( "End of run." );
 } // end main
